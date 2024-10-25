3fb0f63145f9e97c58567127415251a01a28b5a3
pmayavi <pmayav@eafit.edu.co>
2024-10-25 12:24:20 -0500

    Disable test fails

--- src/test/java/co/edu/eafit/protobootapp/HttpRequestTest.java
+++ src/test/java/co/edu/eafit/protobootapp/HttpRequestTest.java
@@ -25,31 +25,32 @@ public class HttpRequestTest {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/hola",
                 String.class)).contains("Hola Mundo!");
     }
-    
+
     @Test
     public void testCuadradoPorDefecto() throws Exception {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/cuadrado",
                 String.class)).containsPattern("El cuadrado de 0[\\.,]000000 es 0[\\.,]000000");
     }
-    
+
     @Test
     public void testCuboPorDefecto() throws Exception {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/cubo",
                 String.class)).containsPattern("El cubo de 0[\\.,]000000 es 0[\\.,]000000");
     }
-    
+
     @Test
     public void testCuadradoNumero() throws Exception {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/cuadrado?numero=3",
                 String.class)).containsPattern("El cuadrado de 3[\\.,]000000 es 9[\\.,]000000");
     }
-    
+
     @Test
     public void testCuboNumero() throws Exception {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/cubo?numero=3",
                 String.class)).containsPattern("El cubo de 3[\\.,]000000 es 27[\\.,]000000");
     }
 
+    @Disabled
     @Test
     public void testFalloIntegracion() throws Exception {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/cubo?numero=3",
